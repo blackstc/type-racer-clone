883f35f609508af9a7d58f35064747563abf762a
Blackstock <charlieblackstock@gmail.com>
2015-09-14 15:54:55 -0600

    got quiz words to translate

--- client/public/js/challengeController.js
+++ client/public/js/challengeController.js
@@ -3,25 +3,34 @@ function($scope, $http, $routeParams) {
   var id = $routeParams.userID;
   $scope.start = true;
   var quizWords = [];
+  $scope.translatedWords = [];
 
   $scope.createQuiz = function() {
-    getRandomWords();
     $scope.start = false;
+    for (var i = 0; i < quizWords.length; i++) {
+      $scope.translate(quizWords[i]);
+    }
   };
 
   //gets 20 random words to be used in the quiz
   getRandomWords = function() {
     $http.get('/api/v1/challenge/' + id)
     .success(function(data) {
-      $scope.words = data;
+      quizWords = data;
     });
   };
 
-  $scope.translate = function() {
-     $http.post("/api/v1/practice/" + id, $scope.newWord)
-     .success(function(data) {
-       $scope.word = data;
-     });
+  $scope.translate = function(word) {
+    payload = {
+      start: $scope.newWord.start,
+      end: $scope.newWord.end,
+      word: word
+    };
+
+    $http.post("/api/v1/practice/" + id, payload)
+    .success(function(data) {
+      $scope.translatedWords.push(data);
+    });
   };
 
   //get request to grab all users in the database
@@ -32,6 +41,7 @@ function($scope, $http, $routeParams) {
     });
   };
 
-
   $scope.getUser();
+  getRandomWords();
+
 }]);
