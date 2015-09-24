c720f5e3d2bf111234333f326b19baad6189cb32
Blackstock <charlieblackstock@gmail.com>
2015-09-24 13:24:41 -0600

    added key listener to check if each word correct on space

--- client/controllers/racingController.js
+++ client/controllers/racingController.js
@@ -1,16 +1,25 @@
 angular.module("app").controller("RacingController", ["$scope", "racingFactory", function($scope, racingFactory) {
   // $scope.words = [];
   $scope.currentWordIndex = 0;
+  $scope.user = {};
 
   // factory call to grab 20 random words from the NPM
   racingFactory.getWords()
   .success(function(data) {
-    console.log(data);
+    $scope.words = data;
   });
 
-  if (checkWord($scope.userInput, $scope.words[$scope.currentWordIndex])) {
-    $scope.userInput = "";
+  $scope.keyPress = function(e) {
+    if (e === 32) {
+     if (checkWord($scope.user.input, $scope.words[$scope.currentWordIndex])) {
+       correct();
+     }
+    }
+  };
+
+  function correct() {
+    $scope.user = {};
     $scope.currentWordIndex++;
-    
+    console.log($scope.currentWordIndex);
   }
 }]);
