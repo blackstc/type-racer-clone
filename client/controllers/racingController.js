b3d0f13951df5c2ca02fa26674382227c0c8b050
Blackstock <charlieblackstock@gmail.com>
2015-09-25 10:01:52 -0600

    added color to words if spelt correctly or incorrectly

--- client/controllers/racingController.js
+++ client/controllers/racingController.js
@@ -2,6 +2,8 @@ angular.module("app").controller("RacingController", ["$scope", "racingFactory",
   // $scope.words = [];
   $scope.currentWordIndex = 0;
   $scope.user = {};
+  $scope.correctWords = [];
+  $scope.incorrectWords = [];
 
   // factory call to grab 20 random words from the NPM
   racingFactory.getWords()
@@ -11,15 +13,50 @@ angular.module("app").controller("RacingController", ["$scope", "racingFactory",
 
   $scope.keyPress = function(e) {
     if (e === 32) {
-     if (checkWord($scope.user.input, $scope.words[$scope.currentWordIndex])) {
-       correct();
-     }
+      if (checkWord($scope.user.input, $scope.words[$scope.currentWordIndex])) {
+        correct();
+      } else {
+        incorrect();
+      }
+    }
+  };
+
+  $scope.checkWord = function(userWord, word) {
+    if (userWord === word) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  $scope.checkCharacter = function(userInput, word) {
+    if (userInput !== undefined) {
+
+      var userArr = userInput.split("");
+      var wordArr = word.split("");
+
+      for (var i = 0; i < userArr.length; i++) {
+        if (userArr[i] !== wordArr[i]) {
+          return true;
+        }
+      }
     }
   };
 
   function correct() {
     $scope.user = {};
+    $scope.correctWords.push($scope.currentWordIndex);
     $scope.currentWordIndex++;
+
     console.log($scope.currentWordIndex);
   }
+
+  function incorrect() {
+    $scope.user = {};
+    $scope.incorrectWords.push($scope.currentWordIndex);
+    console.log($scope.incorrectWords);
+    $scope.currentWordIndex++;
+  }
+
+
 }]);
